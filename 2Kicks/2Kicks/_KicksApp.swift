50d856fd7f7c5749bfc1b79a5af26334ae9ea541
Tavon Gibbs <tavongibbs@Tavons-MacBook-Air.local>
2021-08-20 12:39:25 -0500

    added project

---
+++ 2Kicks/2Kicks/_KicksApp.swift
@@ -0,0 +1,17 @@
+//
+//  _KicksApp.swift
+//  2Kicks
+//
+//  Created by Tavon Gibbs on 8/18/21.
+//
+
+import SwiftUI
+
+@main
+struct _KicksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
